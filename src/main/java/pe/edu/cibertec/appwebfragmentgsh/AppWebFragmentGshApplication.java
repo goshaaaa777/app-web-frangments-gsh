c7767ae54e675a3080da6128773a843a4d1c2fa9
goshaaaa777 <isaiasddx322z@gmail.com>
2023-10-01 15:33:56 -0500

    inicio proyecto

---
+++ src/main/java/pe/edu/cibertec/appwebfragmentgsh/AppWebFragmentGshApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragmentgsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFragmentGshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFragmentGshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebfragmentgsh/AppWebFragmentGshApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragmentgsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragmentGshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
